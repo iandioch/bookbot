40937ac722842e141bd1302f2f022cd9a547f53e
iandioch <iandioch11@gmail.com>
2017-11-13 14:50:27 -0500

    Add ignore for *.priv.*

--- bookbot.py
+++ bookbot.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 PAT = None
-with open('config.txt', 'r') as f:
+with open('config.priv.txt', 'r') as f:
     lines = f.readlines()
     PAT = lines[0].strip()
 VERIFICATION_TOKEN = ''
