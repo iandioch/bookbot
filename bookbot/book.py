b9a29fbf65f406a48f0dc0d6127f17a9814c01ca
iandioch <iandioch11@gmail.com>
2017-11-13 16:23:01 -0500

    Add book.py

---
+++ bookbot/book.py
@@ -0,0 +1,25 @@
+import json
+
+
+class BookSource:
+    """Somewhere this book can be found. Eg. A specific library, a person, or a URL."""
+
+    def __init__(self):
+        pass
+
+    def is_available(self):
+        return False
+
+
+class Book:
+    """An object containing data about a book."""
+
+    def __init__(self):
+        self.title: str = None
+        self.authors: List[str] = []
+        self.sources: List[BookSource] = []
+        self.sources.append('hi')
+        pass
+
+    def __repr__(self) -> str:
+        return json.dumps(self.__dict__)
