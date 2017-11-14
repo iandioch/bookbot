e57f4e212422b0f7c9e48e7f99952147ab18d836
iandioch <iandioch11@gmail.com>
2017-11-13 20:59:18 -0500

    Add 'View my books' view

--- bookbot/view.py
+++ bookbot/view.py
@@ -10,8 +10,9 @@ db = TinyDB('data/users.priv.json')
 User = Query()
 
 # Quick Replies
-QR_SEARCH_FOR_BOOK = "Search for book"
-QR_I_OWN_THIS_BOOK = "I have a copy"
+QR_SEARCH_FOR_BOOK = 'Search for book'
+QR_I_OWN_THIS_BOOK = 'I have a copy'
+QR_VIEW_MY_BOOKS = 'View my books'
 
 
 def create_quick_reply(s, payload=""):
@@ -64,7 +65,7 @@ class BookSearchView(View):
 class BookDetailView(View):
     """A view giving details about a particular book."""
 
-    quick_replies = [QR_I_OWN_THIS_BOOK, QR_SEARCH_FOR_BOOK]
+    quick_replies = [QR_I_OWN_THIS_BOOK, QR_SEARCH_FOR_BOOK, QR_VIEW_MY_BOOKS]
 
     @classmethod
     def search(cls, user_id, query):
@@ -76,7 +77,6 @@ class BookDetailView(View):
         message = '*{}* ({})\n'.format(book.title, book.year)
         message += ', '.join('_{}_'.format(a) for a in book.authors)
         message += '\nISBN: {}'.format(book.isbn)
-        # cls.show_info(user_id, message, create_quick_replies(cls.quick_replies))
         payload = json.dumps({'book': {'isbn':book.isbn}})
         quick_reply_objs = create_quick_replies(cls.quick_replies, payload)
         cls.show_info(user_id, message, quick_reply_objs)
@@ -90,7 +90,7 @@ class OwnBookView(View):
     """A view after a user says they own a copy of a book."""
 
     message = 'Ok!'
-    quick_replies = [QR_SEARCH_FOR_BOOK]
+    quick_replies = [QR_VIEW_MY_BOOKS, QR_SEARCH_FOR_BOOK]
 
     @staticmethod
     def show(user_id, payload):
@@ -104,23 +104,52 @@ class OwnBookView(View):
             print('ERROR: No isbn in book in payload.')
             super(OwnBookView, OwnBookView).show(user_id)
             return
-        users_data = db.search(User.id == user_id)
+        users_data = db.search(User.fb_id == user_id)
         user_data = None
         if len(users_data) > 0:
             user_data = users_data[0]
         if user_data is None:
-            user_data = {'id': user_id}
+            user_data = {'fb_id': user_id}
+            db.insert(user_data)
         if 'owns' not in user_data:
             user_data['owns'] = []
         user_data['owns'].append(data['book']['isbn'])
-        db.update(user_data, User.id == user_id)
+        db.update(user_data, User.fb_id == user_id)
         super(OwnBookView, OwnBookView).show(user_id)
 
+
+class MyBooksView(View):
+    """A view where a user can see a list of all of the
+    books that they have a copy of."""
+
+    quick_replies = [QR_SEARCH_FOR_BOOK]
+    message = "You haven't marked any books as owned!"
+
+    @staticmethod
+    def show(user_id, payload):
+        print('viewing books of', user_id)
+        users = db.search(User.fb_id == user_id)
+        books = []
+        if len(users) > 0:
+            user = users[0]
+            if 'owns' in user:
+                books = user['owns']
+
+        if len(books) == 0:
+            super(MyBooksView, MyBooksView).show(user_id)
+            return
+
+        message = ''
+        for isbn in books:
+            book = Book.search_book(isbn)
+            message += '\n*{}* (ISBN `{}`)'.format(book.title, book.isbn)
+        MyBooksView.show_info(user_id, message, create_quick_replies(MyBooksView.quick_replies))
 
 
 view_triggers = {
         QR_SEARCH_FOR_BOOK: BookSearchView,
         QR_I_OWN_THIS_BOOK: OwnBookView,
+        QR_VIEW_MY_BOOKS: MyBooksView,
 }
 
 
